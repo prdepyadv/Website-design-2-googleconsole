2e2a4c40144ead7c120ecbf8c2c9c13d5c15e9fb
prdepyadv <prdepyadv@gmail.com>
2020-01-28 15:38:59 +0530

    Update phone_upd.php

--- phone_upd.php
+++ phone_upd.php
@@ -3,6 +3,7 @@
 	$phone = $_POST["phone"];
 	$email = $_POST["email"];
 
+if(isset($phone) and isset($email)){
 
 $host="34.69.140.33";$dbusername="rooot";$dbpassword="Prdep@123";$dbname="practice";
 
@@ -23,9 +24,14 @@ $host="34.69.140.33";$dbusername="rooot";$dbpassword="Prdep@123";$dbname="practi
 						echo "Error: ".$sql."<br>".$conn->error;
 						}
 			$conn->close();
-		}
-   
+	}
+	 die();
+}
+else{
+echo "<script>alert('error')</script>","<script>window.location.replace('http://34.69.140.33/Website-design-2/index.php')</script>";
+ die();
+}
     
-    die();	
+   	
 
 ?>
