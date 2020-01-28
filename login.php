bd0fcd73e6e295dca6bbac84dec8b57ae006625a
prdepyadv <prdepyadv@gmail.com>
2020-01-28 12:00:26 +0530

    validate isset pas and email

--- login.php
+++ login.php
@@ -2,6 +2,8 @@
 
 	$email = $_POST["email"];
 	$password = $_POST["password"];
+if(isset($email) and isset($password))
+{
 
 $host="34.69.140.33";$dbusername="rooot";$dbpassword="Prdep@123";$dbname="practice";
     
@@ -16,7 +18,13 @@ $host="34.69.140.33";$dbusername="rooot";$dbpassword="Prdep@123";$dbname="practi
 			$result_e = mysqli_query($conn,$sql_e);
 			echo mysqli_num_rows($result_e);
 		}
-   
+
+}
+else
+{
+echo "<script>alert('error')</script>","<script>window.location.replace('http://34.69.140.33/Website-design-2/index.php')</script>";
+}
+
     die();	
 
 ?>
