9aae8143a01922475d1881042b04d7f5379839bf
prdepyadv <prdepyadv@gmail.com>
2020-01-28 12:14:49 +0530

    Create image_upload.php

---
+++ image_upload.php
@@ -0,0 +1,39 @@
+<?php
+
+if(isset($_POST["submit"])){
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if($check !== false){
+        $image = $_FILES['image']['tmp_name'];
+        $imgContent = addslashes(file_get_contents($image));
+
+        /*
+         * Insert image data into database
+         */
+        
+        $host     = '34.69.140.33';
+        $dbUsername = 'rooot';
+        $dbPassword = 'Prdep@123';
+        $dbName     = 'practice';
+        
+        //Create connection and select DB
+        $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+        
+        // Check connection
+        if($db->connect_error){
+            die("Connection failed: " . $db->connect_error);
+        }
+        
+        $dataTime = date("Y-m-d H:i:s");
+        
+        //Insert image content into database
+        $insert = $db->query("INSERT into images (image, created) VALUES ('$imgContent', '$dataTime')");
+        if($insert){
+            echo "File uploaded successfully.";
+        }else{
+            echo "File upload failed, please try again.";
+        } 
+    }else{
+        echo "Please select an image file to upload.";
+    }
+}
+?>
