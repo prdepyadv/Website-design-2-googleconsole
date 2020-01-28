8524b84affc608c8e5f875d73b29d0c67e9d9223
prdepyadv <prdepyadv@gmail.com>
2020-01-28 15:45:53 +0530

    Update image_upload.php

--- image_upload.php
+++ image_upload.php
@@ -1,40 +1,39 @@
 <?php
 
-    $check = getimagesize($_FILES["image"]["tmp_name"]);
-console.log($check);
-    if($check !== false){
-        $image = $_FILES['image']['tmp_name'];
-        $imgContent = addslashes(file_get_contents($image));
+	$email = $_POST["email"];
+	$image = $_POST["image"];
 
-        /*
-         * Insert image data into database
-         */
-        
-        $host     = '34.69.140.33';
-        $dbUsername = 'rooot';
-        $dbPassword = 'Prdep@123';
-        $dbName     = 'practice';
-        
-        //Create connection and select DB
-        $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-        
-        // Check connection
-        if($db->connect_error){
-            die("Connection failed: " . $db->connect_error);
-        }
-        
-        $dataTime = date("Y-m-d H:i:s");
-        
-        //Insert image content into database
-        $insert = $db->query("INSERT into images (image, created) VALUES ('$imgContent', '$dataTime')");
-        if($insert)
-        {
-            echo 1;
-        }
-		else
+if(isset($email)){
+
+$host="34.69.140.33";$dbusername="rooot";$dbpassword="Prdep@123";$dbname="practice";
+
+    $conn=new mysqli ($host ,$dbusername,$dbpassword,$dbname);
+	if(mysqli_connect_error())
+	    {
+			die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+	    }
+	else
 		{
-			echo "Error: ".$sql."<br>".$conn->error;
-		}
-						$conn->close();
-    }
+		$random_digit=rand(0000,9999);
+		$new_file_name=$random_digit.$file_name;
+		path= "upload/".$new_file_name;
+            $sql="UPDATE Test SET image_path = '".$new_file_name."' WHERE email = '".$email."';";
+		
+					if($conn->query($sql))
+						{
+							echo true;
+						}
+					else
+						{
+						echo "Error: ".$sql."<br>".$conn->error;
+						}
+			$conn->close();
+	}
+	 die();
+}
+else{
+echo "<script>alert('error')</script>","<script>window.location.replace('http://34.69.140.33/Website-design-2/index.php')</script>";
+ die();
+}
+  
 ?>
