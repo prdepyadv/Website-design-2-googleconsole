801229795f3795ababab1922c396012cc9320fae
prdepyadv <prdepyadv@gmail.com>
2020-01-29 18:53:02 +0530

    Update success.php

--- success.php
+++ success.php
@@ -413,6 +413,7 @@ function all_image_show()
 				    url: 'http://34.69.140.33/Website-design-2/all_images.php',
 				    type: 'post',
 				    async: false,
+				    data: {'email' : "<?php echo $email ?>"},
 				    success: function(response)
 				    {	
 					 console.log(response);
