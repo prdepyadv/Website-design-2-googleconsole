78d6024b0d9d029afff7a4c3d1c93cd3a9863098
prdepyadv <prdepyadv@gmail.com>
2020-01-29 12:34:55 +0530

    Update image_upload.php

--- image_upload.php
+++ image_upload.php
@@ -17,7 +17,12 @@ if(isset($email)){
 		$random_digit=rand(0000,9999);
 		$new_file_name=$random_digit.$file_name;
 		$path= "/home/varun544v/".$new_file_name;
-		move_uploaded_file($_FILES["pic"]["tmp_name"] , $path);
+		if(move_uploaded_file($_FILES["pic"]["tmp_name"] , $path))
+		{echo "The file has been uploaded.";}
+		else
+		{echo "Sorry, there was an error uploading your file.";}
+		
+		
             $sql="UPDATE Test SET image_path = '".$new_file_name."' WHERE email = '".$email."';";
 		
 					if($conn->query($sql))
