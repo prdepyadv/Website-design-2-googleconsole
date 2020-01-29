9c5a5e3536ffe4de00593f8712b2e37c56dd3c48
prdepyadv <prdepyadv@gmail.com>
2020-01-29 18:30:39 +0530

    Update all_images.php

--- all_images.php
+++ all_images.php
@@ -7,15 +7,14 @@ $dbname="practice";
 $conn=new mysqli ($host ,$dbusername,$dbpassword,$dbname);
 
 	if(mysqli_connect_error())
-	    {
+	       {
 			die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
-	    }
+	        }
 	else
 		{
 			$sql_e = "SELECT image_path FROM Test";
 			$result_e = mysqli_query($conn,$sql_e);
 			$row = mysqli_fetch_assoc($result_e);
 			echo "/Website-design-2/uploads/".$row["image_path"];
-			}
 		}
 ?>
