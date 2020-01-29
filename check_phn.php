d0c6c379c3500b739c000155b14c6b393102f2d3
prdepyadv <prdepyadv@gmail.com>
2020-01-29 13:55:01 +0530

    Update check_phn.php

--- check_phn.php
+++ check_phn.php
@@ -19,7 +19,7 @@ if(isset($email))
 			$row = mysqli_fetch_assoc($result_e);
 			if(! is_null ( $row["Phone"] ))
 			{
-				$sql_e = "SELECT Phone FROM Test WHERE email = '".$email."'";
+				$sql_e = "SELECT image_path FROM Test WHERE email = '".$email."'";
 				$result_e = mysqli_query($conn,$sql_e);
 				$row = mysqli_fetch_assoc($result_e);
 				echo $row["image_path "];	
