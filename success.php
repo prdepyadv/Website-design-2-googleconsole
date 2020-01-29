4c471c36d5554cdc468a331e99e962da8d79c225
prdepyadv <prdepyadv@gmail.com>
2020-01-29 13:29:55 +0530

    Update success.php

--- success.php
+++ success.php
@@ -269,8 +269,8 @@ Update info (2/2)
 <div class="main_cont">
 <form id="pform3" onsubmit="">
 
-<h1 class="pull">WELCOME</h1> 
-<img class="pull" src="/var/www/html/Website-design-2/uploads/7147-background-4232859_1280.png" style="width:20px;height:20px">
+<h1 >WELCOME</h1> 
+<img src="/var/www/html/Website-design-2/uploads/7147-background-4232859_1280.png" style="width:40px;height:40px">
 </form>
 </div>
 
