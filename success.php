3e0048581e04a4d0b3a00157e03f2d4053042115
prdepyadv <prdepyadv@gmail.com>
2020-01-30 18:16:43 +0530

    Update success.php

--- success.php
+++ success.php
@@ -412,15 +412,18 @@ function all_data_show(){
 					    data: {'username' : "<?php echo $username ?>"},
 					    success: function(response)
 					    {	
-						 document.getElementById("mytable").insertRow(0).innerHTML = '<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Password</th><th>Date of Birth</th><th>Register Date</th><th>Phone Number</th><th>Image Path</th><th>Username</th></tr>';
-						 data = response.split("\n");
-						 var i=0;
-						 while(i<data.length - 1)
-						{
-							var row = data[i].split("*");
-							document.getElementById("mytable").insertRow(-1).innerHTML = '<td>'+row[0]+'</td><td>'+row[1]+'</td><td>'+row[2]+'</td><td>'+row[3]+'</td><td>'+row[4]+'</td><td>'+row[5]+'</td><td>'+row[6]+'</td><td>'+row[7]+'</td><td>'+row[8]+'</td><td>'+row[9]+'</td><td><input type="button" value="Edit" onclick="editRow(this)">/<input type="button" value="Delete" onclick="deleteRow(this)"></td>';
-							i=i+1;
-						}
+						 if(response)
+						 {
+							 document.getElementById("mytable").insertRow(0).innerHTML = '<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Password</th><th>Date of Birth</th><th>Register Date</th><th>Phone Number</th><th>Image Path</th><th>Username</th></tr>';
+							 data = response.split("\n");
+							 var i=0;
+							 while(i<data.length - 1)
+							{
+								var row = data[i].split("*");
+								document.getElementById("mytable").insertRow(-1).innerHTML = '<td>'+row[0]+'</td><td>'+row[1]+'</td><td>'+row[2]+'</td><td>'+row[3]+'</td><td>'+row[4]+'</td><td>'+row[5]+'</td><td>'+row[6]+'</td><td>'+row[7]+'</td><td>'+row[8]+'</td><td>'+row[9]+'</td><td><input type="button" value="Edit" onclick="editRow(this)">/<input type="button" value="Delete" onclick="deleteRow(this)"></td>';
+								i=i+1;
+							}
+						 }
 					    }
 					});
 	}
