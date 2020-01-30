2be545063d06c33417d7d0842c838fdadd3fdc69
prdepyadv <prdepyadv@gmail.com>
2020-01-30 15:56:43 +0530

    Update all_data.php

--- all_data.php
+++ all_data.php
@@ -1,1 +1,30 @@
+<?php
 
+$username = $_POST["username"];
+
+if(isset($username))
+{
+	if($username == "prdepyadv")
+	{
+              $host="34.69.140.33";
+              $dbusername="rooot";
+              $dbpassword="Prdep@123";
+              $dbname="practice";
+              $conn=new mysqli ($host ,$dbusername,$dbpassword,$dbname);
+
+                if(mysqli_connect_error())
+                       {
+                    die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+                  }
+                else
+                  {
+                    $sql_e = "SELECT * FROM Test";
+                    $result_e = mysqli_query($conn,$sql_e);
+                    while ($row = mysqli_fetch_assoc($result_e))
+                    {						
+                      echo $row."\n";
+                    }
+                  }
+	}	
+}
+?>
