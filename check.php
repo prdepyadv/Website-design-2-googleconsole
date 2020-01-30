616a0688bea4bbf9a01bf181a7cf200867881885
prdepyadv <prdepyadv@gmail.com>
2020-01-30 13:03:29 +0530

    Update check.php

--- check.php
+++ check.php
@@ -31,6 +31,14 @@ if(isset($email) and isset($password))
 					$encryption_iv = '1234567891011121';
 					$encryption_key = "prdepyadv";
 					$password = openssl_encrypt($password, $ciphering, $encryption_key, $options, $encryption_iv); 
+			
+				/*
+					$decryption_iv = '1234567891011121'; 
+					$decryption_key = "prdepyadv
+					$password = openssl_decrypt ($password, $ciphering, 
+					$decryption_key, $options, $decryption_iv); 	
+				*/
+				
 					$sql="INSERT INTO Test(first,last,email,password,dob,username) VALUES ('$first','$last','$email','$password','$dob','$username')";
 						if($conn->query($sql))
 						{
