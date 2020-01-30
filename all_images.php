b41d31c6972756dbb0ca62f844333f221d6fdfb5
prdepyadv <prdepyadv@gmail.com>
2020-01-30 15:14:31 +0530

    Update all_images.php

--- all_images.php
+++ all_images.php
@@ -21,11 +21,8 @@ if(isset($username))
 					$sql_e = "SELECT image_path FROM Test";
 					$result_e = mysqli_query($conn,$sql_e);
 					while ($row = mysqli_fetch_assoc($result_e))
-					{
-						if($row["image_path"] != " ")
-						{						
-							echo "/Website-design-2/uploads/".$row["image_path"]."\n";
-						}
+					{						
+						echo "/Website-design-2/uploads/".$row["image_path"]."\n";
 					}
 				}
 	}	
