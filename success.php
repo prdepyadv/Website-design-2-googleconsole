0548cbb95022668ebd2b71e0580623911a7c254e
prdepyadv <prdepyadv@gmail.com>
2020-02-04 17:58:10 +0530

    Update success.php

--- success.php
+++ success.php
@@ -454,7 +454,7 @@ function deleteRow(r) {
 	
 function editRow(r) {
   var i = r.parentNode.parentNode.rowIndex;
-  $("#mytable:nth-child("+i+")").prop('contenteditable', true);
+  $("#mytable:nth-child("+r+")").prop('contenteditable', true);
 }
 	
 function all_image_show()
