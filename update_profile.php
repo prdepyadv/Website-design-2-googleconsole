2b20a4f71c77707f90ec06c16b3d8d3148d03fb5
prdepyadv <prdepyadv@gmail.com>
2020-02-04 18:43:10 +0530

    Update and rename edit_row.php to update_profile.php

---
+++ update_profile.php
@@ -0,0 +1,42 @@
+<?php
+
+$id = $_POST["id"];
+ $first = $_POST["first"];
+$last = $_POST["last"];
+$email = $_POST["email"];
+$dob = $_POST["dob"];
+$Phone = $_POST["Phone"];
+	$username = $_POST["username"];
+	
+
+if(isset($username)){
+
+	$host="35.226.43.57";$dbusername="rooot";$dbpassword="Prdep@123";$dbname="practice";
+
+    $conn=new mysqli ($host ,$dbusername,$dbpassword,$dbname);
+	if(mysqli_connect_error())
+	    {
+			die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+	    }
+	else
+		{
+            $sql="UPDATE Test SET first = '".$first."' WHERE id = '".$id."';";
+		
+					if($conn->query($sql))
+						{
+							echo true;
+						}
+					else
+						{
+						echo "Error: ".$sql."<br>".$conn->error;
+						}
+			$conn->close();
+	}
+	 die();
+}
+else{
+echo "<script>alert('error')</script>","<script>window.location.replace('http://35.226.43.57/Website-design-2/index.php')</script>";
+ die();
+}
+  
+?>
