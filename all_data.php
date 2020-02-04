22966d3509ea87dc790b45ad9fe12fb3e15bcc64
prdepyadv <prdepyadv@gmail.com>
2020-02-04 13:06:27 +0530

    Update all_data.php

--- all_data.php
+++ all_data.php
@@ -22,8 +22,7 @@ if(isset($username))
 		    $upload = array();
                     $result_e = mysqli_query($conn,$sql_e);
                     while ($row = mysqli_fetch_assoc($result_e))
-                    {	
-			    
+                    {	 
 			    $upload + = array("id" => $row["id"],"first" => $row["first"],"last" => $row["last"],"email" => $row["email"],"password" => $row["password"],"dob" => $row["dob"],"register_date" => $row["register_date"],"Phone" => $row["Phone"], "image_path" => $row["image_path"], "username" => $row["username"]);
                     }
 			echo json_encode($upload);
