e2e41efe6b46730ead091dd8465780fb49f61da9
prdepyadv <prdepyadv@gmail.com>
2020-02-05 10:30:35 +0530

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -14,22 +14,33 @@ if(isset($username)){
 
     $conn=new mysqli ($host ,$dbusername,$dbpassword,$dbname);
 	if(mysqli_connect_error())
-	    {
+	{
 			die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
-	    }
+	 }
 	else
+	{
+            	
+		$sql_e = "SELECT * FROM Test WHERE email = '".$email."'";
+		$result_e = mysqli_query($conn,$sql_e);
+		$i = mysqli_num_rows($result_e);
+		if($i == 0)
 		{
-            $sql="UPDATE Test SET first = '".$first."', last= '".$last."', email= '".$email."', dob = '".$dob."', Phone = '".$phone."' WHERE id = '".$id."'";
-		
-					if($conn->query($sql))
-						{
-							echo true;
-						}
-					else
-						{
-						echo "Error: ".$sql."<br>".$conn->error;
-						}
-			$conn->close();
+			$sql="UPDATE Test SET first = '".$first."', last= '".$last."', email= '".$email."', dob = '".$dob."', Phone = '".$phone."' WHERE id = '".$id."'";
+
+						if($conn->query($sql))
+							{
+								echo true;
+							}
+						else
+							{
+							echo "Error: ".$sql."<br>".$conn->error;
+							}
+		}
+		else
+		{
+			echo false;
+		}
+		$conn->close();
 	}
 	 die();
 }
